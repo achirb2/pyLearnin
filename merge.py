8377eb5e238de7ada5a6bd7c3790ffa46cc43294
Al Chirban <alexander.chirban@gmail.com>
2018-10-01 23:13:14 -0500

    added the split inversion counting and cleaned up code for readability and commenting

--- merge.py
+++ merge.py
@@ -12,13 +12,29 @@ def mergeSort(A):
         return merge(left, right)
 
 
-def merge(A, B):
+def merge(A, B, C = None): 
+#   create list for returning at end of function
     outList = []
+#   initialize indexes for A and B lists
     Ai = 0
     Bi = 0
+#   set flags to show if A or B lists have been exhausted
     Agood = True
     Bgood = True
-    for i in range( len(A + B) ):
+#   Total length of both lists combined
+    N = len(A + B)
+
+#   initialize splitInv to 0 if C is passed a value
+    if C is not None:
+        splitInv = 0
+    else:
+        splitInv = None
+
+#   Loop N times to fill output array
+    for i in range( N ):
+#   Try to pull lowest number from each input list
+#       If an Index Error is found then stop pulling
+#       from that list
         try:
             if A[Ai]:
                 Agood = True
@@ -29,7 +45,7 @@ def merge(A, B):
                 Bgood = True
         except IndexError:
             Bgood = False
-        
+#   Check if both lists are still good. If so compare them        
         if Agood and Bgood:
             if A[Ai] <= B[Bi]:
                 outList.append(A[Ai])
@@ -37,13 +53,23 @@ def merge(A, B):
             elif B[Bi] <= A[Ai]:
                 outList.append(B[Bi])
                 Bi += 1
+
+#   Count inversions if flagged to do so
+                if splitInv is not None:
+                    splitInv = splitInv + ((N/2) - i + 1)
+#   if B is exhausted just add As
         elif Agood:
             outList.append(A[Ai])
             Ai += 1
+#   if A is exhausted just add Bs
         elif Bgood:
             outList.append(B[Bi])
             Bi += 1
 
-    print(outList)
-    return outList
+#   return tuple with splitInv converted to int if flagged
+    if splitInv is not None:
+        return (outList, int(splitInv))
+#   otherwise return just the output list
+    else:
+        return outList
 
